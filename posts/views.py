2b81307a8781f34d8821e45db14404b8b03fe189
gvLeonardo <gv_leonardo@outlook.com>
2020-09-29 17:21:34 -0600

    Cambios de 23/09

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
 
+# utilities
+# Lib datetime de django
+from datetime import datetime
+
 # Create your views here.
+
+posts = [
+    {
+        'title': 'Picture from Nietzy',
+        'user': {
+            'name': 'Nietzy Cardoza',
+            'picture': 'https://picsum.photos/60/60'
+        },
+        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/300/300',
+    },
+    {
+        'title': 'Picture from Crecencio',
+        'user': {
+            'name': 'Crecencio Gameros',
+            'picture': 'https://picsum.photos/60/60'
+        },
+        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/300/300',
+    },
+    {
+        'title': 'Picture from Brenda',
+        'user': {
+            'name': 'Brenda Gutierrez',
+            'picture': 'https://picsum.photos/60/60'
+        },
+        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/300/300',
+    },
+    {
+        'title': 'Picture from Luis',
+        'user': {
+            'name': 'Luis Hernandez',
+            'picture': 'https://picsum.photos/60/60'
+        },
+        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/300/300',
+    },
+    {
+        'title': 'Picture from Vianey',
+        'user': {
+            'name': 'Vianey Lopez',
+            'picture': 'https://picsum.photos/60/60'
+        },
+        'timestamp': datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/300/300',
+    }
+]
+
+def list_post (request):
+    return render(request,'feed.html',{'posts': posts})
--- utgram/urls.py
+++ utgram/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+# importando las vistas
+from posts import views as posts_views
+
 def hello_world(request):
-    return HttpResponse('<h1>Hola mundo deff</h1>')
+    return HttpResponse('<h1>Hola mundosdasdasd</h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+
+    # PostViews
+    path('feed/', posts_views.list_post)
 ]
