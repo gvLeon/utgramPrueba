fa29284338d0f2b3bd48b018fe59152cf95b6f76
gvLeonardo <gv_leonardo@outlook.com>
2020-09-29 20:42:39 -0600

    User model guardado

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+""" Users Models """
 
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    """Profile model
+        Proxi model that extends data with other information
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
