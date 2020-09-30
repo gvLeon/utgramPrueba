a37ec3468c68cfb0d2ac2f73cdc26c5da038c2d7
gvLeonardo <gv_leonardo@outlook.com>
2020-09-29 19:39:04 -0600

    Modelo posts agregado

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
+""" Post Models """
+
 from django.db import models
 
 # Create your models here.
+class Post (models.Model):
+
+    title = models.CharField(max_length=255)
+    # Required pillow library
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
+
