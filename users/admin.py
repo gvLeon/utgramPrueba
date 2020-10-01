ce286a14366786f3f05cbf38ffd3d2f1087628c4
gvLeonardo <gv_leonardo@outlook.com>
2020-10-01 14:24:57 -0600

    Clase del miercoles 30

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,4 @@
 """ Users admin config """
-
 # Django
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
@@ -9,18 +8,42 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 
-# Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
 
-    # informacion a mostrar
-    list_display=('id','user','birthdate','website','picture')
+    # Que es lo que quiero que se muestre
+    list_display = ('id', 'user', 'birthdate', 'website', 'picture')
+    # Que elementos son clickables
+    list_display_links = ('id', 'user')
+    # Que elementos se pueden editar ahi mismo
+    list_editable = ('birthdate', 'website', 'picture')
+    # Como quieres buscar un elemento
+    search_fields = ('user__email', 'user__is_staff', 'created', 'modified')
+    # Filtro de datos
+    list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
 
-    # Elementos clickeables
-    list_display_links=('id','user')
 
-    # Elementos editables ahi mismo
-    list_editable=('birthdate','website')
+class ProfileInline(admin.StackedInline):
+    """ Profile in-line admin for users """
 
-    # Buscar elementos
-    search_fields=('user__email','user__is_staff','created','modified')
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """ Add profile admin to base user admin """
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+    list_editable=('is_active', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
--- users/models.py
+++ users/models.py
@@ -3,28 +3,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Profile(models.Model):
-    """Profile model
+    """ Profile model
         Proxi model that extends data with other information
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     picture = models.ImageField(
-        upload_to = 'users/pictures',
-        blank = True,
+        upload_to = 'users/pictrues',
+        blank= True,
         null = True
     )
 
-    # https://leonardogaribaldidev.com
-    website = models.URLField(max_length=255, blank=True)
-
+    
+    website = models.URLField(max_length = 255, blank=True)
     birthdate = models.DateField(blank=True, null=True)
-
     bio = models.TextField(blank=True, null=True)
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add = True)
+    modified = models.DateTimeField(auto_now = True)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.user)
