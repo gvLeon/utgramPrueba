91f217948f9d379bf6634fc1b8a60877aebf82f4
gvLeonardo <gv_leonardo@outlook.com>
2020-10-09 01:31:03 -0600

    5 oct video 2

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+''' Users views '''
 
-# Create your views here.
+# Django
+# Libreria para ayudarnos a hacer el login del usuario y 'login' para manejar la sesion del usuario por ej al cambiar de pestana se queda la sesion iniciada
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+def login_view(request):
+    '''Login view '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
